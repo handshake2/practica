28e0a60195941bb7b3ccf0dbb556af30a8eaf637
handshake2 <programerperu@gmail.com>
2018-08-30 21:41:30 -0500

    Initial Commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,23 @@
-io();
+//const socket=io();
+$(function (){
+    //alert('works');
+    const socket=io();
+
+    const $messageForm = $('#message-form');
+    const $messageBox = $('#message');
+    const $chat = $('#chat');
+
+    $messageForm.submit( e => {
+        e.preventDefault();
+        console.log($messageBox.val());
+        socket.emit('send message', $messageBox.val());
+        //socket.emit('send message', $messageBox.val(), data => {
+          //$chat.append(`<p class="error">${data}</p>`)
+        //});
+        $messageBox.val('');
+      });
+      socket.on('new message',function(data){
+        $chat.append(data + '<br/>');
+    });
+    })
+    
